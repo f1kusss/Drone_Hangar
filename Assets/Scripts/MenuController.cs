f6e6cc27fc83947b861509cd13a89f62948125da
Nickolay <strategne.ru@mail.ru>
2023-10-16 14:31:59 +0300

    init

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -29,7 +29,7 @@ public class MenuController : MonoBehaviour
         head = MainCam.GetComponent<Vrs.Internal.VrsHead>();
         prevCamPos = MainCam.transform.position;
         preCamOrient = MainCam.transform.rotation;
-        //switchFPV();
+        switchFPV();
     }
 
     void Update()
